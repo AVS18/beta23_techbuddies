c8a498f58fadb33040b235659b7dbed2f0ca047e
A V S ADITYAVARDHAN <47914143+AVS18@users.noreply.github.com>
2020-10-31 13:25:12 +0530

    Add files via upload

--- base/admin.py
+++ base/admin.py
@@ -1,13 +1,5 @@
-<<<<<<< HEAD
 from django.contrib import admin
 from .models import User, SiteAnnouncements
 # Register your models here.
 admin.site.register(User)
 admin.site.register(SiteAnnouncements)
-=======
-from django.contrib import admin
-from .models import User, SiteAnnouncements
-# Register your models here.
-admin.site.register(User)
-admin.site.register(SiteAnnouncements)
->>>>>>> 7804fdbc8beb87c0011895981248dcd449f93126
